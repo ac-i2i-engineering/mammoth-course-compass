2e4a60608a3db643c4e52f2136cac451d092241d
Liam Davis <davisliam123@gmail.com>
2024-11-08 21:13:01 +0000

    updated config to make app prod ready

--- access_amherst_backend/access_amherst_backend/settings.py
+++ access_amherst_backend/access_amherst_backend/settings.py
@@ -19,13 +19,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
-    "django-insecure-g#^63*v$=mo#n=z##(nfa9v+m^c)u4^+92b3p3t#0xxpd-=j-7"
+    os.getenv("DJANGO_SECRET_KEY")
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['access-amherst-a59g.onrender.com']
 
