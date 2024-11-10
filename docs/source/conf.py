335a20cf93c3abbb26c12e41ba412dd81bc2e38f
Thu Hoang <thuhoang.hannah@gmail.com>
2024-11-10 14:13:35 -0500

    Change the documentation theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,8 @@ language = "python"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["../build/html/_static"]
+html_theme = "sphinx_rtd_theme"
+#html_static_path = ["../build/html/_static"]
 
 # set up Django environment
 import os
