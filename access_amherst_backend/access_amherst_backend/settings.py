ad0a60116d119ccac543fd1a8ce029f20e289d08
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2024-11-12 09:04:21 -0800

    Update ALLOWED_HOSTS to new render domain

--- access_amherst_backend/access_amherst_backend/settings.py
+++ access_amherst_backend/access_amherst_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["access-amherst-a59g.onrender.com"]
+ALLOWED_HOSTS = ["access-amherst-jbqe.onrender.com"]
 
 # Application definition
 
