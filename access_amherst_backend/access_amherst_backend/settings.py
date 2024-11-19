ef87828323a16dd05a448344f216a7f9a770e356
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2024-11-19 14:33:16 -0800

    add new host from GoDaddy to Allowed Hosts

--- access_amherst_backend/access_amherst_backend/settings.py
+++ access_amherst_backend/access_amherst_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["access-amherst-jbqe.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["access-amherst-jbqe.onrender.com", "amherst-connect.com","127.0.0.1"]
 
 # Application definition
 
